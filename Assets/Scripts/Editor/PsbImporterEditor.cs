71f91b46a16d520e5a2c2cb7c00a4b22c40a9e50
Flzzyf <2981019122@qq.com>
2019-12-26 18:00:27 +0800

    1

---
+++ Assets/Scripts/Editor/PsbImporterEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+
+[CustomEditor(typeof(PsbImporter))]
+public class PsbImporterEditor : ScriptedImporterEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PsbImporter main = target as PsbImporter;
+
+        //serializedObject.Update();
+
+        //ApplyRevertGUI();
+
+        if(GUILayout.Button("qwe"))
+        {
+            string prefabPath = "Assets/qwe.prefab";
+            EditorUtility.CreateEmptyPrefab(prefabPath);
+
+            AnimationClip clip = new AnimationClip();
+            clip.name = "anim";
+            AssetDatabase.AddObjectToAsset(clip, prefabPath);
+
+            EditorUtility.CreateEmptyPrefab("Assets/qwe2.prefab");
+            AssetDatabase.AddObjectToAsset(clip, prefabPath);
+        }
+    }
+}
